415a0e82f75a10a7e792eb8f6b8436ac91bd96ea
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-26 12:24:15 +0200

    Corretto jsnack 4

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -1,23 +1,28 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 //  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-const partyList = ["Marco", "Paolo", "Giulio", "Riccardo", "Alessandro"];
+const partyList = ["Alessandro", "Roberto", "Marco", "Luigi"];
 
-const userName = prompt("Inserisci il tuo nome per verificare se è in lista!");
+// Chiedo la mail all'utente
+const userName = prompt("Inserisci il tuo nome!");
 
+// Verifico che la mail sia nella lista e faccio sapere l'esito all'utente
 for (let i = 0; i < partyList.length; i++) {
     const element = partyList[i];
 
     if(userName === element){
-        alert("Hai il permesso per entrare!");
+        alert("Sei sulla lista puoi entrare!!");
 
         break;
-    }else if(userName != element && element === partyList.length){
-        alert("Spiacente non puoi entrare!");
+    }
+    else{
+        alert("Non sei sulla lista spiacente!");
 
         break;
     }
 }
+
+window.location.reload();
 
 
 
