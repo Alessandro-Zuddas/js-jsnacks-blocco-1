fba34d1217021d37fb3f79f76276b309646462f4
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-26 10:51:23 +0200

    first commit

---
+++ jsnack2/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const firstWord = prompt("Inserisci la prima parola!");
+const secondWord = prompt("Inserisci la seconda parola!");
+
+if(firstWord.length > secondWord.length){
+    console.log(firstWord, secondWord);
+}else{
+    console.log(secondWord, firstWord)
+}
