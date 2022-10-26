18f15f5602cecb7fed28b36d87bb3fd51097adf1
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-26 12:30:05 +0200

    jsnack 5

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,1 +1,17 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
 "use strict";
+
+const emptyArray = [];
+
+for (let i = 0; i <= 6; i++) {
+    const element = emptyArray[i];
+    const userInput = Number(prompt("Inserisci un numero!"));
+
+    if(userInput % 2 == 1){
+        emptyArray.push(userInput);
+        console.log(emptyArray);
+    }
+}
